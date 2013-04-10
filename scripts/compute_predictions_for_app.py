0196a8a56197ac39e76ed48ae411e8dd0beb90c4
Stephan Gabler <stephan.gabler@gmail.com>
2013-04-10 14:44:10 +0200

    add OOB predictions to the data used in the web app

--- scripts/compute_predictions_for_app.py
+++ scripts/compute_predictions_for_app.py
@@ -31,15 +31,20 @@ import itertools as it
 from master.libs import run_lib as rl
 from master.libs import utils
 import numpy as np
+import pybel
+import copy
 reload(rl)
 
-outpath = os.path.join(os.path.dirname(__file__), '..', '..', 'models')
+mol_file = '/Users/dedan/projects/master/data/molecules.sdf'
+outpath = '/Users/dedan/projects/odor_app/models'
 data_path = os.path.join(os.path.dirname(__file__), '..', 'data')
-door2id = json.load(open(os.path.join(data_path, 'door2id.json')))
-id2door = {v[0]: k for k, v in door2id.items()}
 gloms = json.load(open(os.path.join(data_path, 'all_glomeruli.json')))
-gloms = gloms[:2]
-
+molecules = pybel.readfile('sdf', mol_file)
+id2desc = {}
+for m in molecules:
+    data = dict(m.data)
+    id2desc[m.data['CdId']] = { 'name': data.get('Name', '')
+                               , 'CID': data.get('PubChem CID', '')}
 base_config = {
     "data_path": data_path,
     "feature_selection": {
@@ -47,13 +52,13 @@ base_config = {
         "method": "linear"
         },
     "features": {
-      "normalize": True,
-      "properties_to_add": []
+        "normalize": True,
+        "properties_to_add": []
     },
     "methods": {
-      "svr": {
-        "C": 1.0,
-        "n_folds": 50
+        "svr": {
+            "C": 1.0,
+            "n_folds": 50
       }
     },
     "randomization_test": False
@@ -65,8 +70,8 @@ feat_config = {
         "descriptor": "haddad_desc",
     },
     "all": {
-      "type": "conventional",
-      "descriptor": "all",
+        "type": "conventional",
+        "descriptor": "all",
     },
     "eva_100": {
       "type": "spectral",
@@ -86,6 +91,8 @@ for k, config in feat_config.items():
     cache[k] = {"features": rl.prepare_features(base_config)}
 all_mols = [r['features'].keys() for r in cache.values()]
 mol_intersection = set(all_mols[0]).intersection(*all_mols[1:])
+for k in cache:
+    cache[k]['features'] = {m: cache[k]['features'][m] for m in mol_intersection}
 
 res = {n: {g: {} for g in gloms} for n in feat_config}
 for glom in gloms:
@@ -102,10 +109,6 @@ for glom in gloms:
             'molids': molids
         }
 
-    # select molecules that none of the models will be trained on
-    all_trained = set(dtm.values()[0]['molids']).union(*[m['molids'] for m in dtm.values()[1:]])
-    to_predict_molids = mol_intersection - all_trained
-
     for name, data in dtm.items():
 
         # fit model
@@ -113,12 +116,31 @@ for glom in gloms:
         base_config['feature_selection']['k_best'] = data['data'].shape[1]
         print("use {} molecules for training".format(data['data'].shape[0]))
         tmp_res = rl.run_runner(base_config, data['data'], data['targets'], get_models=True)
-        to_predict = np.array([cache[name]['features'][molid] for molid in to_predict_molids])
-        res[name][glom]['predictions'] = tmp_res[method]['model'].predict(to_predict)
-        res[name][glom]['cases'] = [id2door[molid] for molid in to_predict_molids]
-        res[name][glom]['targets'] = data['targets']
-        res[name][glom]['score'] = tmp_res[method]['gen_score']
+
+        res[name][glom]['predictions'] = []
+        molid2oob = {}
+        for i, molid in enumerate(data['molids']):
+            molid2oob[molid] = {}
+            molid2oob[molid]['oob_mean'] = round(float(np.mean([x[0] for x in tmp_res[method]['model'].all_predictions[i]])), 3)
+            molid2oob[molid]['oob_var'] = round(float(np.var([x[0] for x in tmp_res[method]['model'].all_predictions[i]])), 3)
+
+        preds = tmp_res[method]['model'].predict(np.array(cache[name]['features'].values()))
+        for i, (m, p) in enumerate(zip(cache[name]['features'].keys(), preds)):
+            tmp = copy.deepcopy(id2desc[m])
+            tmp.update({
+                'prediction': round(float(p), 3),
+                'molid': m
+            })
+
+            if m in data['molids']:
+                tmp['target'] = round(float(data['targets'][data['molids'].index(m)]), 3)
+                tmp['oob_prediction'] = molid2oob[m]
+            res[name][glom]['predictions'].append(tmp)
+        if tmp_res[method]['gen_score'] > 0:
+            res[name][glom]['score'] = round(float(tmp_res[method]['gen_score']), 3)
+        else:
+            res[name][glom]['score'] = 0
+
         print('model genscore: {:.2f}\n'.format(tmp_res[method]['gen_score']))
 
 pickle.dump(dict(res), open(os.path.join(outpath, 'predictions.pkl'), 'w'))
-
